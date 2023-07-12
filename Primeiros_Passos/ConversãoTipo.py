d067e47f3f8fd84f36e510574688671f00e37b14
Perkles <g.n.d2014@gmail.com>
2023-07-12 20:16:11 -0300

    First Commit

---
+++ Primeiros_Passos/ConversãoTipo.py
@@ -0,0 +1,45 @@
+'''Conversão de Inteiro para Float'''
+
+preco = 10
+print (preco,"numero inteiro")
+
+preco = float(preco)
+print(preco,"numero float")
+
+
+'''Conversão de Float para Inteiro'''
+
+preco = 10.30
+print (preco,"numero float")
+
+preco = int(preco)
+print(preco,"numero inteiro")
+
+
+'''Conversão por divisao'''
+
+preco = 10
+print(preco)
+
+print(preco/2,"numero float")
+print(preco//2,"numero inteiro")
+
+'''Conversão numérico para string'''
+
+preco = 10.50
+idade = 28
+
+print(str(preco))
+print(str(idade))
+
+texto = f"idade {idade} preco {preco}"
+print(texto)
+
+''' Conversão de String para Número'''
+
+preco = "10.53"
+idade = "28"
+
+print (float(preco))
+
+print(int(idade))
---
+++ Primeiros_Passos/Variaveis_Constantes.py
@@ -0,0 +1,9 @@
+nome = "Cristiane"
+idade = 23
+
+ESTADO_BRASIL = "SP"
+
+print(nome,idade)
+
+print ("Eu sou o", nome, "e tenho",idade,"e moro em",ESTADO_BRASIL)
+
