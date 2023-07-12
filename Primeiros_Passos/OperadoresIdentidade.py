8de58746f992d23d89795110e3ccf4a10b23abef
Perkles <g.n.d2014@gmail.com>
2023-07-12 20:26:49 -0300

    Operadores de Identidade

---
+++ Primeiros_Passos/OperadoresIdentidade.py
@@ -0,0 +1,7 @@
+saldo = 1000
+limite = 1000
+
+print(saldo is limite)
+print ( saldo is not limite)
+
+# o IS é um operador de identidade que nos mostra se uma variavel ocupa a mesma regiao de memoria de outra
