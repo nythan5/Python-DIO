bbf1c8376be009c54dfa4a44d4a599385dca481c
Perkles <g.n.d2014@gmail.com>
2023-07-19 14:26:45 -0300

    Metodos em Strings

---
+++ ManipulacaoStrings/MetodosString.py
@@ -0,0 +1,24 @@
+#Maiuscula
+curso = "PytHon"
+print(curso.upper())
+
+#Minuscula
+print(curso.lower())
+
+#Titulo
+print(curso.title())
+
+#Removendo espaços do texto e integrando strings
+
+texto = " Olá Mundo!   "
+
+print(texto.strip())
+print(texto.lstrip())
+print(texto.rstrip())
+
+inicio = "Home"
+
+print(inicio.center(12,"#"))
+
+print(".".join(inicio))
+
