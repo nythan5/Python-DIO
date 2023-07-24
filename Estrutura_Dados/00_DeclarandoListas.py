b016e502916d3eaabe733d42310c6ec3bdb913fe
Perkles <g.n.d2014@gmail.com>
2023-07-24 15:34:28 -0300

    Listas

---
+++ Estrutura_Dados/00_DeclarandoListas.py
@@ -0,0 +1,14 @@
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+frutas = []#lista vazia pois nao necessariamente ela precisa ter algo para ser declarada
+print(frutas)
+
+letras = list("python")#cada letra será um elemento ['p', 'y', 't', 'h', 'o', 'n']
+print(letras)
+
+numeros = list(range(10))# utilizamos o construtor list com range para fazer uma lista de 0 a 9 sem a necessidade de digitar todos os elementos
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
