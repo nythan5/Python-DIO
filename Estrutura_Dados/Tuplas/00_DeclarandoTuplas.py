e670eb4fba406f57c36f717b1cfc7fb37abc3841
Perkles <g.n.d2014@gmail.com>
2023-07-31 20:43:32 -0300

    Aula de Tuplas

---
+++ Estrutura_Dados/Tuplas/00_DeclarandoTuplas.py
@@ -0,0 +1,17 @@
+#tupla é uma lista imutavél quase uma constante e uma tupla é declarada usando ()
+
+frutas = (
+    "laranja",
+    "pera",
+    "uva",
+)
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
