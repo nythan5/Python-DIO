2633ab67adb9b3e26efbaf9756e170a075e4232d
Perkles <g.n.d2014@gmail.com>
2023-08-08 16:45:05 -0300

    Funcoes

---
+++ Estrutura_Dados/Dicionario/00_Declarando.py
@@ -0,0 +1,8 @@
+pessoa = {"nome": "Gabriel", "idade": 23}
+print(pessoa)
+
+pessoa = dict(nome="Gabriel", idade=23)
+print(pessoa)
+
+pessoa["telefone"] = "3333-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
+print(pessoa)
---
+++ Estrutura_Dados/Dicionario/01_Acessando.py
@@ -0,0 +1,11 @@
+dados = {"nome": "Gabriel", "idade": 23, "telefone": "3333-1234"}
+
+print(dados["nome"])  # "Gabriel"
+print(dados["idade"])  # 23
+print(dados["telefone"])  # "3333-1234"
+
+dados["nome"] = "Maria"
+dados["idade"] = 18
+dados["telefone"] = "9988-1781"
+
+print(dados)  # {"nome": "Maria", "idade": 18, "telefone": "9988-1781"}
