142e1b7100fa37f0f440562b97b42c6a9344eb15
Perkles <g.n.d2014@gmail.com>
2023-08-22 21:34:32 -0300

    Melhorando a bicicletaria

--- Programacao_Orientada_a_Objetos/Classes&Objetos/bicicletaria.py
+++ Programacao_Orientada_a_Objetos/Classes&Objetos/bicicletaria.py
@@ -1,22 +1,47 @@
 class bicicleta :
-    def __init__(self,cor,modelo,tamanho,valor):
-        self.cor = cor
+    def __init__(self,cor,modelo,valor,aro): #self é o construtor
+        self.cor = cor  # isso que é self. são os atributos da classe
         self.modelo = modelo
-        self.tamanho = tamanho
-        self.valor = valor
+        self.aro = aro
+        self.valor = valor        
+        self.marcha = 0
 
-    def buzinar(self):
+
+    def buzinar(self): #metodo é identico a funcao
         print("Bi! Bi!")
 
     def grau(self):
         print("BOLOLOLO")
 
-    def __str__(self):
-        return f"{self.__class__.__name__}:{','.join([f'{chave}={valor}' for chave,valor in self.__dict__.items()])}"
+    def aumentar_marcha(_self,n_marcha):
+        if n_marcha > _self.marcha:
+            print("Marcha trocada")
+        else:
+            print("Tente uma marcha seguinte")
+
+    def diminuir_marcha(_self,n_marcha):
+        if n_marcha < _self.marcha:
+            print("Marcha Reduzida")
+        else:
+            print("Tente uma marcha anterior")        
     
 
-bike = bicicleta("Vermelha","BMX","29",1000)
+    def __str__(self):
+        return f"{self.__class__.__name__}:{', '.join([f'{chave}={valor}' for chave,valor in self.__dict__.items()])}"
+    
 
-print(bike.cor)
+bike1 = bicicleta("Vermelha","BMX",1000,"29")
 
-bike.grau()
+bike2 = bicicleta("Azul","GTS",2000,"29")
+
+#print(bike1.cor)
+
+#bike1.grau() # este cara é igual a linha debaixo
+
+#bicicleta.grau(bike2)
+
+#print(bike1) # aqui estamos utilizando o metodo STR
+
+bike1.aumentar_marcha(1)
+bike1.aumentar_marcha(2)
+bike1.diminuir_marcha(-1)
