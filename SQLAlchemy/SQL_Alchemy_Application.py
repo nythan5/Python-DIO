389e99bc5a8de03dbdb4436a37b88f773dffe0e2
nythan5 <g.n.d2014@gmail.com>
2023-09-05 11:18:19 -0300

    Executando Funções de Statements na aplicação

--- SQLAlchemy/SQL_Alchemy_Application.py
+++ SQLAlchemy/SQL_Alchemy_Application.py
@@ -49,8 +49,6 @@ class Address(Base):
         return f"Address(id={self.id}, email_address={self.email_address})"
 
 
-print(User.__tablename__)
-print(Address.__tablename__)
 
 # conexão com o banco de dados
 engine = create_engine("sqlite://")
@@ -61,6 +59,72 @@ Base.metadata.create_all(engine)
 
 # investiga o esquema de banco de dados
 inspetor_engine = inspect(engine)
-print(inspetor_engine.has_table("user_account"))
+#print(inspetor_engine.has_table("user_account"))
 print(inspetor_engine.get_table_names())
-print(inspetor_engine.default_schema_name)
+#print(inspetor_engine.default_schema_name)
+
+
+# Criando uma sessão ao BD para persistir dados
+with Session(engine) as session: 
+    gabriel = User (
+        name= "Gabriel",
+        fullname = "Gabriel Nathan Dias",
+        address= [Address(email_address="gabriel@email.com")]
+    )
+
+#Criando objetos para serem persistidos 
+    cristiane = User (
+        name= "Cristiane",
+        fullname = "Cristiane Guimarães",
+        address= [Address(email_address="cristiane@email.com"),
+                  Address(email_address="cristianeguimaraes@email.com")]
+    )
+
+    ota = User (name= "ota",fullname = "ota ota")
+
+
+# Para submeter as os registros
+session.add_all([gabriel,cristiane,ota])
+session.commit()
+
+
+# Recuperando a informações do BD
+stmt_user = select(User).where(User.name.in_(["Gabriel","ota"])) 
+print("\n","Recuperando Usuário")
+for user in session.scalars(stmt_user):
+    print(user)
+
+stmt_address = select(Address).where(Address.user_id.in_([2]))
+print("\n","Recuperando Email usuário Cristiane")
+for address in session.scalars(stmt_address):
+    print(address)
+
+
+# Realizando consultas com o Order by
+order_by = select(User).order_by(User.name.desc())
+print("\n","Recuperando Usuários com ORDER BY")
+for user in session.scalars(order_by):
+    print(user)
+
+
+# Realizando consultas com o Join (junção entre as tabelas)
+join_stmt = select(User.id,User.fullname,Address.email_address).join_from(Address,User)
+print("\n","Recuperando Usuários com JOIN")
+for result in session.scalars(join_stmt):
+    print(result)
+
+# Desta maneira /\ ele so tras o primeiro atributo no caso o "User.id"
+
+# Executando o Join Statement apartir da connection e nao da session para obter todos os atributos
+connection = engine.connect()
+resultados = connection.execute(join_stmt).fetchall()
+print("\n","Recuperando todos os atributos do JOIN")
+for result in resultados:
+    print(result)
+
+
+# Função COUNT
+count = select(func.count("*")).select_from(User)
+print("\n","Recuperando Quantidade de registro na tabela USER")
+for result in session.scalars(count):
+    print(result)
