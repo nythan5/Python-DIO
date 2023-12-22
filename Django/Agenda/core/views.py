279e2603c3d6d60f6b8eddeeed7cebc2a5fe59c3
G15 <g.n.d2014@gmail.com>
2023-12-22 19:17:05 -0300

    Inserindo informações no BD

---
+++ Django/Agenda/core/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from core.models import Evento
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+# Create your views here.
+
+
+@login_required(login_url='/login/')
+def lista_eventos(request):
+    usuario = request.user
+    eventos = Evento.objects.filter(usuario=usuario)
+    response = {'eventos': eventos}
+    return render(request, 'agenda.html', response)
+
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        print(usuario)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, message="Usuario ou senha inválidos")
+
+
+    return redirect('/')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+
+        Evento.objects.create(titulo=titulo, data_evento=data_evento, descricao=descricao, usuario=usuario)
+
+    return redirect('/')
